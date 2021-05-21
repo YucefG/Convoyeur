2e03eacd9170049aee1111fdc2a9105eda68aa94
lemirachehab <lemira.chehab@epfl.ch>
2021-05-21 19:03:49 +0200

    ajout nouvelle lumiere

---
+++ Convoyeur-main/lumiere.c
@@ -0,0 +1,78 @@
+#include "ch.h"
+#include "hal.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <motors.h>
+#include <chprintf.h>
+
+#define CENT				100
+#define DEUX_CENTS			200
+
+// Cette fonction eteint toutes les lumieres
+void lumiere_eteinte(void)
+{
+
+	palSetPad(GPIOD, GPIOD_LED1);
+	palSetPad(GPIOD, GPIOD_LED3);
+	palSetPad(GPIOD, GPIOD_LED5);
+	palSetPad(GPIOD, GPIOD_LED7);
+
+}
+
+
+void lumiere_clignote(void)
+{
+	palTogglePad(GPIOD, GPIOD_LED1);
+	palTogglePad(GPIOD, GPIOD_LED3);
+	palTogglePad(GPIOD, GPIOD_LED5);
+	palTogglePad(GPIOD, GPIOD_LED7);
+}
+
+
+//Cette fonction allume toutes les lumieres
+void lumiere_allumee(void)
+{
+	palClearPad(GPIOD, GPIOD_LED1);
+	palClearPad(GPIOD, GPIOD_LED3);
+	palClearPad(GPIOD, GPIOD_LED5);
+	palClearPad(GPIOD, GPIOD_LED7);
+}
+//Cette fonction est un jeu de lumiere lorsque le selecteur est a 0
+void jeu_de_lumiere(void)
+{
+	palClearPad(GPIOD, GPIOD_LED1);
+	palClearPad(GPIOD, GPIOD_LED5);
+
+	chThdSleepMilliseconds(100);// Regler la rapidite du jeu de lumiere
+
+    palSetPad(GPIOD, GPIOD_LED1);
+    palClearPad(GPIOD, GPIOD_LED5);
+
+    chThdSleepMilliseconds(100);
+
+	palClearPad(GPIOD, GPIOD_LED3);
+	palClearPad(GPIOD, GPIOD_LED7);
+
+	chThdSleepMilliseconds(100);
+
+    palSetPad(GPIOD, GPIOD_LED3);
+    palClearPad(GPIOD, GPIOD_LED5);
+
+    palClearPad(GPIOB, GPIOB_LED_BODY);
+
+    chThdSleepMilliseconds(200);
+
+    palSetPad(GPIOB, GPIOB_LED_BODY);
+    lumiere_allumee();
+
+   	chThdSleepMilliseconds(200);
+   	lumiere_eteinte();
+
+   	palClearPad(GPIOB, GPIOB_LED_BODY);
+
+    palClearPad(GPIOD, GPIOD_LED1);
+    lumiere_allumee();
+}
+
---
+++ Convoyeur-main/lumiere.h
@@ -0,0 +1,9 @@
+#ifndef LUMIERE_H
+#define LUMIERE_H
+
+void lumiere_eteinte(void);
+void lumiere_clignote(void);
+void  lumiere_allumee(void);
+void jeu_de_lumiere(void);
+
+#endif /* LUMIERE_H */
